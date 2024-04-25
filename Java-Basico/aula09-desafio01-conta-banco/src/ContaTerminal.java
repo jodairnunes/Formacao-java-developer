9740a16c8199bc651bf31f5760a138fa7cc518e0
jodair <jodairnunes@hotmail.com>
2024-04-25 18:26:01 -0300

    entrega do desafio

--- Java-Basico/aula09-desafio01-conta-banco/src/ContaTerminal.java
+++ Java-Basico/aula09-desafio01-conta-banco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: Conhecer e importar a classe Scanner
+
+        // Criação de um objeto Scanner para ler entrada do teclado
+        Scanner scanner = new Scanner(System.in);
+
+        //Exibir as mensagens para o nosso usuário
+        System.out.println("Por favor, digite o número de sua Agência: ");
+        //Obter pela scanner os valores digitados no terminal
+        String agencia = scanner.nextLine();
+        System.out.println("Por favor, digite o seu nome: ");
+        String nomeCliente = scanner.nextLine();
+        double saldo = 100;
+        int usuario = 1021;
+
+
+        //Exibir a mensagem conta criada
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + usuario + " e seu saldo R$" + saldo + " já está disponível para saque");
+        
+        // Não esqueça de fechar o Scanner
+        scanner.close();
     }
 }
